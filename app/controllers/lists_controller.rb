fb995b49b8a529c2f119439a5c9e63b24ef19db6
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-08-13 14:45:55 -0600

    fill out #create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,16 +7,18 @@ class ListsController < ApplicationController
     def show 
         @list = List.find(params[:id])
     end
-    def new 
 
-    end
     def create 
+        @list = List.new(list_params)
+        @list.name = params[:list][:name]
+        @list.save
 
+        redirect_to list_url(@list)
     end
 
 
     private 
     def list_params
-        
+        params.require(:list).permit(:name)
     end
 end
