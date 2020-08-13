9c9ecd88e968f21b707324e0ff950efe3c664912
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-08-13 14:34:33 -0600

    add form for new list item in index page. add #create and private #list_params to list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,9 +1,22 @@
 class ListsController < ApplicationController
     def index
+        @list = List.new 
         @lists = List.all
 
     end
     def show 
         @list = List.find(params[:id])
+    end
+    def new 
+
+    end
+    def create 
+
+    end
+
+
+    private 
+    def list_params
+        
     end
 end
