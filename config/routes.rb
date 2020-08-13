f73e3e9f346af7ac351c15904a93f117fbf50718
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-08-13 14:05:55 -0600

    take html template from todomvc.com and strip down to eccensials, create site controller, static. create list resource. Migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :lists
+  get 'site/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'site#index'
 end
--- notes.md
+++ notes.md
@@ -1,4 +1,5 @@
 # step 1: integrate basic theme to aset pipeline 
+    - see a stubbed out , non-dynamic page with the theme
 # step 2: Allow people to CRUD lists
     -create a list
     -see all their lists
